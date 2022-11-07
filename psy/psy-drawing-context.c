e6e8718ca34d5a8181ae64eadcc5036fb37c6d0d
Maarten Duijndam <m.j.a.duijndam@uu.nl>
2022-11-07 09:46:06 +0100

    Elaborate  api of drawing context
    Add functions to register and obtain PsyPrograms
    Split shaders in vertex and fragment shaders

--- psy/psy-drawing-context.c
+++ psy/psy-drawing-context.c
@@ -1,8 +1,10 @@
 
 
 #include "psy-drawing-context.h"
+#include "psy-program.h"
 #include "psy-shader.h"
 #include "psy-vbuffer.h"
+#include "psy-enums.h"
 
 /**
  * PsyDrawingContext:
@@ -17,7 +19,10 @@
  * render pictures or shapes in an arbitrary color.
  */
 
+G_DEFINE_QUARK(psy-context-error-quark, psy_context_error)
+
 typedef struct _PsyDrawingContextPrivate {
+    GHashTable *shader_programs;
 } PsyDrawingContextPrivate;
 
 G_DEFINE_ABSTRACT_TYPE_WITH_PRIVATE(PsyDrawingContext, psy_drawing_context, G_TYPE_OBJECT)
@@ -68,7 +73,12 @@ static void
 psy_drawing_context_init(PsyDrawingContext *self)
 {
     PsyDrawingContextPrivate* priv = psy_drawing_context_get_instance_private(self);
-    (void) priv;
+    priv->shader_programs = g_hash_table_new_full(
+            g_str_hash,
+            g_str_equal,
+            g_free,
+            g_object_unref
+            );
 }
 
 static void
@@ -76,7 +86,11 @@ psy_drawing_context_dispose(GObject* object)
 {
     PsyDrawingContext* self = PSY_DRAWING_CONTEXT(object);
     PsyDrawingContextPrivate* priv = psy_drawing_context_get_instance_private(self);
-    (void) priv;
+
+    if (priv->shader_programs) {
+        g_hash_table_destroy(priv->shader_programs);
+        priv->shader_programs = NULL;
+    }
 
     G_OBJECT_CLASS(psy_drawing_context_parent_class)->dispose(object);
 }
@@ -105,6 +119,73 @@ psy_drawing_context_class_init(PsyDrawingContextClass* class)
 
 /* ************ public functions ******************** */
 
+
+/**
+ * psy_drawing_context_register:
+ * @self: an instance of `PsyDrawingContext`
+ * @name: the name to use to register the program, it should not have been used
+ *        to register a `PsyProgram` before.
+ * @program: The program which you'd like to have registered. To make sure
+ *           the program works with this context, you should use
+ *           `psy_drawing_context_create` to make it.
+ * @error:(out):If an error occurs such as using the same name twice will be
+ *              returned here.
+ *
+ * Register an Shader program with this context. The program is registered
+ * with the drawing context for future use. The context just stores the program,
+ * so you'll have to add the necessary shaders and do compilation steps yourself.
+ */
+void
+psy_drawing_context_register_program (
+        PsyDrawingContext* self,
+        const gchar* name,
+        PsyProgram* program,
+        GError** error
+        )
+{
+    g_return_if_fail(PSY_IS_DRAWING_CONTEXT(self));
+    g_return_if_fail(name);
+    g_return_if_fail(PSY_IS_PROGRAM(program));
+    g_return_if_fail(error == NULL || *error != NULL);
+
+    PsyDrawingContextPrivate* priv = psy_drawing_context_get_instance_private(self);
+
+    if (g_hash_table_lookup(priv->shader_programs, name) != NULL) {
+        g_set_error(error,
+                PSY_DRAWING_CONTEXT_ERROR,
+                PSY_DRAWING_CONTEXT_ERROR_NAME_EXISTS,
+                "A program with the name %s has already been registered.",
+                name
+                );
+        return;
+    }
+    g_hash_table_insert(
+            priv->shader_programs,
+            g_strdup(name),
+            program
+            );
+}
+
+/**
+ * psy_drawing_context_get_program:
+ * @self: an instance of `PsyDrawingContext`
+ * @name: the name used to register the Program
+ *
+ * Obtain a PsyProgram previously that was previously registered.
+ *
+ * Returns: an Instance of `PsyProgram` or NULL
+ */
+PsyProgram*
+psy_drawing_context_get_program(PsyDrawingContext* self, const gchar* name)
+{
+    g_return_val_if_fail(PSY_IS_DRAWING_CONTEXT(self), NULL);
+    g_return_val_if_fail(name, NULL);
+
+    PsyDrawingContextPrivate* priv = psy_drawing_context_get_instance_private(self);
+
+    return g_hash_table_lookup(priv->shader_programs, name);
+}
+
 PsyProgram*
 psy_drawing_context_create_program (PsyDrawingContext* self)
 {
@@ -116,13 +197,23 @@ psy_drawing_context_create_program (PsyDrawingContext* self)
 }
 
 PsyShader*
-psy_drawing_context_create_shader (PsyDrawingContext* self)
+psy_drawing_context_create_vertex_shader (PsyDrawingContext* self)
 {
     g_return_val_if_fail(PSY_IS_DRAWING_CONTEXT(self), NULL);
     PsyDrawingContextClass* cls = PSY_DRAWING_CONTEXT_GET_CLASS(self);
 
-    g_return_val_if_fail(cls->create_shader, NULL);
-    return cls->create_shader(self);
+    g_return_val_if_fail(cls->create_vertex_shader, NULL);
+    return cls->create_vertex_shader(self);
+}
+
+PsyShader*
+psy_drawing_context_create_fragment_shader (PsyDrawingContext* self)
+{
+    g_return_val_if_fail(PSY_IS_DRAWING_CONTEXT(self), NULL);
+    PsyDrawingContextClass* cls = PSY_DRAWING_CONTEXT_GET_CLASS(self);
+
+    g_return_val_if_fail(cls->create_fragment_shader, NULL);
+    return cls->create_fragment_shader(self);
 }
 
 PsyVBuffer*
@@ -131,7 +222,7 @@ psy_drawing_context_create_vbuffer (PsyDrawingContext* self)
     g_return_val_if_fail(PSY_IS_DRAWING_CONTEXT(self), NULL);
     PsyDrawingContextClass* cls = PSY_DRAWING_CONTEXT_GET_CLASS(self);
 
-    g_return_val_if_fail(cls->create_shader, NULL);
+    g_return_val_if_fail(cls->create_vbuffer, NULL);
     return cls->create_vbuffer(self);
 }
 
--- psy/psy-drawing-context.h
+++ psy/psy-drawing-context.h
@@ -9,6 +9,10 @@
 
 G_BEGIN_DECLS
 
+#define PSY_DRAWING_CONTEXT_ERROR psy_drawing_context_error_quark()
+G_MODULE_EXPORT GQuark
+psy_drawing_context_error_quark(void);
+
 #define PSY_TYPE_DRAWING_CONTEXT psy_drawing_context_get_type()
 G_DECLARE_DERIVABLE_TYPE(PsyDrawingContext, psy_drawing_context, PSY, DRAWING_CONTEXT, GObject)
 
@@ -16,7 +20,8 @@ typedef struct _PsyDrawingContextClass {
     GObjectClass parent_class;
 
     PsyProgram* (*create_program)(PsyDrawingContext* self);
-    PsyShader* (*create_shader)(PsyDrawingContext* self);
+    PsyShader* (*create_vertex_shader)(PsyDrawingContext* self);
+    PsyShader* (*create_fragment_shader)(PsyDrawingContext* self);
     PsyVBuffer* (*create_vbuffer)(PsyDrawingContext* self);
 
 } PsyDrawingContextClass;
@@ -26,11 +31,21 @@ G_MODULE_EXPORT PsyProgram*
 psy_drawing_context_create_program(PsyDrawingContext* self);
 
 G_MODULE_EXPORT PsyShader*
-psy_drawing_context_create_shader(PsyDrawingContext* self);
+psy_drawing_context_create_vertex_shader(PsyDrawingContext* self);
+
+G_MODULE_EXPORT PsyShader*
+psy_drawing_context_create_fragment_shader(PsyDrawingContext* self);
 
 G_MODULE_EXPORT PsyVBuffer*
 psy_drawing_context_create_vbuffer(PsyDrawingContext* self);
 
+G_MODULE_EXPORT void
+psy_drawing_context_register_programe (
+        PsyDrawingContext* self,
+        const gchar* name,
+        PsyProgram* program,
+        GError** error
+        );
 
 G_END_DECLS
 
