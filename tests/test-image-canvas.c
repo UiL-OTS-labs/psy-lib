23944f19f8b108317e6f2526b687244c4a7a4db8
Maarten Duijndam <m.j.a.duijndam@uu.nl>
2023-03-26 15:06:24 +0200

    Update scaling tests

---
+++ tests/test-image-canvas.c
@@ -0,0 +1,86 @@
+
+#include <math.h>
+#include <psylib.h>
+
+const int WIDTH  = 1920;
+const int HEIGHT = 1080;
+
+gfloat g_rotation   = 0.0f;
+gint   g_num_frames = 10;
+
+void
+debug_message(PsyGlCanvas *self,
+              guint        source,
+              guint        type,
+              guint        id,
+              guint        severity,
+              gchar       *message,
+              gchar       *source_str,
+              gchar       *type_str,
+              gchar       *severity_str,
+              gpointer     user_data)
+{
+    (void) source;
+    (void) type;
+    (void) id;
+    (void) severity;
+    (void) self;
+    (void) user_data;
+    g_printerr("%s: message: %s, source: %s, type: %s, severity: %s\n",
+               __func__,
+               message,
+               source_str,
+               type_str,
+               severity_str);
+}
+
+void
+update_rectangle(PsyVisualStimulus *stim,
+                 PsyTimePoint      *tp,
+                 gint64             p0,
+                 gpointer           data)
+{
+    (void) data;
+    (void) tp;
+    g_print("p0 = %ld\n", p0);
+    psy_visual_stimulus_set_rotation(stim, g_rotation);
+    g_rotation += 2 * M_PI / g_num_frames;
+}
+
+int
+main(void)
+{
+    GError         *error   = NULL;
+    PsyDuration    *stimdur = psy_duration_new_ms(1000);
+    PsyImageCanvas *icanvas = PSY_IMAGE_CANVAS(
+        psy_gl_canvas_new_full(WIDTH, HEIGHT, FALSE, TRUE, 4, 4));
+    PsyCanvas *canvas     = PSY_CANVAS(icanvas); // alias
+    PsyColor  *stim_color = psy_color_new_rgb(0.25, .5, .75);
+
+    g_signal_connect(canvas, "debug-message", G_CALLBACK(debug_message), NULL);
+
+    PsyTimePoint *tp_null = psy_time_point_new();
+
+    PsyRectangle *rect = psy_rectangle_new_full(
+        PSY_CANVAS(canvas), 0, 0, WIDTH / 2.0, HEIGHT / 2.0);
+
+    g_signal_connect(rect, "update", G_CALLBACK(update_rectangle), NULL);
+
+    psy_stimulus_play_for(PSY_STIMULUS(rect), tp_null, stimdur);
+    psy_visual_stimulus_set_color(PSY_VISUAL_STIMULUS(rect), stim_color);
+
+    for (int i = 0; i < g_num_frames; i++) {
+        char path[128];
+        g_snprintf(
+            path, sizeof(path), "/%s/some-picture-%d.png", g_get_tmp_dir(), i);
+        psy_image_canvas_iterate(icanvas);
+        PsyImage *image = psy_canvas_get_image(canvas);
+        psy_image_save_path(image, path, "png", &error);
+        g_object_unref(image);
+    }
+
+    g_object_unref(icanvas);
+    g_object_unref(tp_null);
+    g_object_unref(stimdur);
+    g_object_unref(stim_color);
+}
--- tests/test-visual-stimulus.c
+++ tests/test-visual-stimulus.c
@@ -1,19 +1,36 @@
 
 #include <CUnit/CUnit.h>
+#include <math.h>
 #include <psylib.h>
 
 #include "unit-test-utilities.h"
 
-static const gint      WIDTH    = 640;
-static const gint      HEIGHT   = 480;
-static PsyImageCanvas *g_canvas = NULL;
+static const gint WIDTH  = 640;
+static const gint HEIGHT = 480;
+
+static PsyImageCanvas *g_canvas     = NULL;
+static PsyColor       *g_stim_color = NULL;
+static PsyColor       *g_bg_color   = NULL;
+static PsyTimePoint   *g_tp_null    = NULL;
 
 static int
 test_visual_stimulus_setup(void)
 {
-    g_canvas = psy_image_canvas_new(WIDTH, HEIGHT);
-    if (!g_canvas)
+    g_debug("Entering %s", __func__);
+    g_canvas     = psy_image_canvas_new(WIDTH, HEIGHT);
+    g_stim_color = psy_color_new_rgb(1, 1, 1);
+    g_bg_color   = psy_color_new_rgb(0, 0, 0);
+    g_tp_null    = psy_time_point_new();
+
+    if (!g_canvas || !g_stim_color || !g_bg_color || !g_tp_null)
         return 1;
+
+    // make random but significantly different colors
+    while (psy_color_equal_eps(g_stim_color, g_bg_color, 0.25)) {
+        psy_color_set_red(g_stim_color, random_double());
+        psy_color_set_green(g_stim_color, random_double());
+        psy_color_set_blue(g_stim_color, random_double());
+    }
 
     return 0;
 }
@@ -21,15 +38,64 @@ test_visual_stimulus_setup(void)
 static int
 test_visual_stimulus_teardown(void)
 {
+    g_debug("Entering %s", __func__);
     g_clear_object(&g_canvas);
+    g_clear_object(&g_stim_color);
+    g_clear_object(&g_bg_color);
+    g_clear_object(&g_tp_null);
 
     return 0;
+}
+
+/**
+ * compute_surface_area_by_color:
+ * @image:(transfer none): The input image
+ * @color:(transfer none): The color that counts as being part of the stimulus
+ *
+ * Count the pixels in @image that have the color specified by @color. Those
+ * pixels are added to the surface.
+ *
+ * Returns: the number of pixels with the specified color.
+ */
+static gint64
+compute_surface_area_by_color(PsyImage *image, PsyColor *color)
+{
+    gint64 count = 0;
+
+    guint8 stim_pixel[4];
+    gint   r, g, b, a;
+    g_object_get(color, "ri", &r, "gi", &g, "bi", &b, "ai", &a, NULL);
+    stim_pixel[0] = (guint8) r;
+    stim_pixel[1] = (guint8) g;
+    stim_pixel[2] = (guint8) b;
+    stim_pixel[3] = (guint8) a;
+
+    guint num_channels = psy_image_get_num_channels(image);
+    guint stride       = psy_image_get_stride(image);
+    guint width        = psy_image_get_width(image);
+    guint height       = psy_image_get_height(image);
+
+    guint8 *row_ptr, *pixel_ptr, *image_ptr = psy_image_get_ptr(image);
+    for (guint row = 0; row < height; row++) {
+        row_ptr = image_ptr + row * stride;
+        for (guint column = 0; column < width; column++) {
+            pixel_ptr = row_ptr + column * num_channels;
+            if (memcmp(pixel_ptr, stim_pixel, sizeof(stim_pixel)) == 0)
+                count++;
+        }
+    }
+    return count;
+}
+
+gdouble
+circle_area(gdouble radius)
+{
+    return radius * radius * M_PI;
 }
 
 static void
 vstim_default_values(void)
 {
-    // Warns about the window parameter is NULL;
     PsyCircle *circle = psy_circle_new(PSY_CANVAS(g_canvas));
 
     CU_ASSERT_PTR_NOT_NULL_FATAL(circle);
@@ -66,10 +132,20 @@ vstim_default_values(void)
 static void
 vstim_scale(void)
 {
-    const gfloat scale = (float) random_double_range(-10, 10);
-    gfloat       x, y;
+    const gfloat  radius       = 50;
+    const gfloat  num_vertices = 100;
+    const gfloat  scale        = (float) random_double_range(1.5, 2.5);
+    PsyDuration  *frame_dur    = psy_canvas_get_frame_dur(PSY_CANVAS(g_canvas));
+    PsyDuration  *stim_dur     = psy_duration_multiply_scalar(frame_dur, 10);
+    PsyTimePoint *tp_start     = psy_time_point_add(g_tp_null, frame_dur);
+    PsyImage     *image        = NULL;
 
-    PsyCircle *circle = psy_circle_new(PSY_CANVAS(g_canvas));
+    gfloat x, y;
+
+    PsyCircle *circle
+        = psy_circle_new_full(PSY_CANVAS(g_canvas), 0, 0, radius, num_vertices);
+    psy_visual_stimulus_set_color(PSY_VISUAL_STIMULUS(circle), g_stim_color);
+    psy_canvas_set_background_color(PSY_CANVAS(g_canvas), g_bg_color);
 
     g_object_set(circle, "scale", scale, NULL);
     g_object_get(circle, "scale_x", &x, "scale_y", &y, NULL);
@@ -85,7 +161,35 @@ vstim_scale(void)
     CU_ASSERT_DOUBLE_EQUAL(x, scale / 2, 0);
     CU_ASSERT_DOUBLE_EQUAL(y, scale * 2, 0);
 
+    g_object_set(circle, "scale", 1.0, NULL);
+    psy_stimulus_play_for(PSY_STIMULUS(circle), tp_start, stim_dur);
+
+    psy_image_canvas_iterate(g_canvas);
+
+    image            = psy_canvas_get_image(PSY_CANVAS(g_canvas));
+    gint64 area      = compute_surface_area_by_color(image, g_stim_color);
+    gfloat comp_area = circle_area(radius);
+    // allow half a pixel radius margin
+    gfloat margin    = circle_area(radius) - circle_area(radius - .5);
+
+    CU_ASSERT_DOUBLE_EQUAL(area, comp_area, margin);
+
+    // double scaling and check whether scaled stimulus has expected surface
+    g_object_set(circle, "scale", 2.0, NULL);
+    psy_image_canvas_iterate(g_canvas);
+
+    g_object_unref(image);
+    image     = psy_canvas_get_image(PSY_CANVAS(g_canvas));
+    area      = compute_surface_area_by_color(image, g_stim_color);
+    comp_area = circle_area(radius * 2);
+    margin    = circle_area(radius * 2) - circle_area((radius * 2) - .5);
+
+    CU_ASSERT_DOUBLE_EQUAL(area, comp_area, margin);
+
+    g_object_unref(image);
     g_object_unref(circle);
+    g_object_unref(tp_start);
+    g_object_unref(stim_dur);
 }
 
 int
