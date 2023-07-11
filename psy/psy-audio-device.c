bbb85facaa453b7266bcf258511ccf0c7ba75370
Maarten Duijndam <m.j.a.duijndam@uu.nl>
2023-07-11 16:33:08 +0200

    Start of general audio device

---
+++ psy/psy-audio-device.c
@@ -0,0 +1,218 @@
+
+
+#include "psy-audio-device.h"
+#include "psy-config.h"
+#include "psy-enums.h"
+
+/**
+ * PsyAudioDevice:
+ *
+ * A PsyAudioDevice is a device that represents an PCI(e), USB, Firewire
+ * on the mother board, etc. type of an audio interface.
+ */
+
+// clang-format off
+G_DEFINE_QUARK(psy-audio-device-error-quark,
+               psy_audio_device_error)
+
+// clang-format on
+
+typedef struct _PsyAudioDevicePrivate {
+    char    *name;
+    guint    sample_rate;
+    gboolean is_open;
+} PsyAudioDevicePrivate;
+
+G_DEFINE_ABSTRACT_TYPE_WITH_PRIVATE(PsyAudioDevice,
+                                    psy_audio_device,
+                                    G_TYPE_OBJECT)
+
+typedef enum {
+    PROP_NULL,
+    PROP_NAME,
+    PROP_IS_OPEN,
+    PROP_SAMPLE_RATE,
+    NUM_PROPERTIES
+} PsyAudioDeviceProperty;
+
+static GParamSpec *audio_device_properties[NUM_PROPERTIES];
+
+static void
+psy_audio_device_set_property(GObject      *object,
+                              guint         prop_id,
+                              const GValue *value,
+                              GParamSpec   *pspec)
+{
+    PsyAudioDevice *self = PSY_AUDIO_DEVICE(object);
+    (void) self;
+    (void) value;
+
+    switch ((PsyAudioDeviceProperty) prop_id) {
+    default:
+        G_OBJECT_WARN_INVALID_PROPERTY_ID(object, prop_id, pspec);
+    }
+}
+
+static void
+psy_audio_device_get_property(GObject    *object,
+                              guint       prop_id,
+                              GValue     *value,
+                              GParamSpec *pspec)
+{
+    PsyAudioDevice        *self = PSY_AUDIO_DEVICE(object);
+    PsyAudioDevicePrivate *priv = psy_audio_device_get_instance_private(self);
+    (void) value;
+    (void) priv;
+
+    switch ((PsyAudioDeviceProperty) prop_id) {
+    default:
+        G_OBJECT_WARN_INVALID_PROPERTY_ID(object, prop_id, pspec);
+    }
+}
+
+static void
+psy_audio_device_init(PsyAudioDevice *self)
+{
+    PsyAudioDevicePrivate *priv = psy_audio_device_get_instance_private(self);
+    priv->is_open               = FALSE;
+    priv->name        = g_strdup(psy_audio_device_get_default_name(self));
+    priv->sample_rate = 48000; // PSY_SAMPLE_RATE_48000;
+}
+
+static void
+psy_audio_device_dispose(GObject *object)
+{
+    PsyAudioDevice        *self = PSY_AUDIO_DEVICE(object);
+    PsyAudioDevicePrivate *priv = psy_audio_device_get_instance_private(self);
+
+    (void) priv; // currently it doesn't have other GObject's
+
+    G_OBJECT_CLASS(psy_audio_device_parent_class)->dispose(object);
+}
+
+static void
+psy_audio_device_finalize(GObject *object)
+{
+    PsyAudioDevice        *self = PSY_AUDIO_DEVICE(object);
+    PsyAudioDevicePrivate *priv = psy_audio_device_get_instance_private(self);
+    (void) priv;
+
+    g_free(priv->name);
+
+    G_OBJECT_CLASS(psy_audio_device_parent_class)->finalize(object);
+}
+
+static void
+audio_device_open(PsyAudioDevice *self, GError **error)
+{
+    (void) error; // Error's might be raised in derived classes (backends).
+    PsyAudioDevicePrivate *priv = psy_audio_device_get_instance_private(self);
+    priv->is_open               = TRUE;
+    g_info("Opened PsyAudioDevice %s", psy_audio_device_get_name(self));
+}
+
+static void
+audio_device_close(PsyAudioDevice *self)
+{
+    PsyAudioDevicePrivate *priv = psy_audio_device_get_instance_private(self);
+    priv->is_open               = FALSE;
+    g_info("Closed PsyAudioDevice %s", psy_audio_device_get_name(self));
+}
+
+static void
+audio_device_set_name(PsyAudioDevice *self, const gchar *name)
+{
+    PsyAudioDevicePrivate *priv = psy_audio_device_get_instance_private(self);
+    g_clear_pointer(&priv->name, g_free);
+    priv->name = g_strdup(name);
+}
+
+static void
+audio_device_set_sample_rate(PsyAudioDevice *self, guint sample_rate)
+{
+    PsyAudioDevicePrivate *priv = psy_audio_device_get_instance_private(self);
+    priv->sample_rate           = sample_rate;
+}
+
+static void
+psy_audio_device_class_init(PsyAudioDeviceClass *class)
+{
+    GObjectClass *gobject_class = G_OBJECT_CLASS(class);
+
+    gobject_class->set_property = psy_audio_device_set_property;
+    gobject_class->get_property = psy_audio_device_get_property;
+    gobject_class->finalize     = psy_audio_device_finalize;
+    gobject_class->dispose      = psy_audio_device_dispose;
+
+    class->open            = audio_device_open;
+    class->close           = audio_device_close;
+    class->set_name        = audio_device_set_name;
+    class->set_sample_rate = audio_device_set_sample_rate;
+}
+
+/* ************ public functions ******************** */
+
+/**
+ * psy_audio_device_new:(constructor)
+ *
+ * Constructs an audio device. This constructor will check which backends
+ * are available on this machine and if so, it will try to get the best one
+ * possible.
+ * The returned device will implement an PsyAudioDevice for one specific
+ * backend.
+ *
+ * Returns: a instance of [class@PsyAudioDevice]
+ */
+PsyAudioDevice *
+psy_audio_device_new(void)
+{
+#if defined(HAVE_JACK)
+    return psy_jack_audio_device_new();
+#elif defined(HAVE_ALSA)
+    return psy_alsa_audio_device_new();
+#endif
+    return NULL;
+}
+
+// TODO
+// /**
+//  * psy_audio_device_get_playback:
+//  * @self: An instance of [class@AudioDevice] that will provide a
+//  * [class@AudioPlayback]
+//  *
+//  * Requests the backend of this audiodevice to provide a
+//  * [class@AudioPlayback] that is compatible with this context.
+//  *
+//  * Returns:(transfer none): An instance of [class@Texture]
+//  */
+// PsyTexture *
+// psy_audio_device_create_playback(PsyAudioDevice *self)
+// {
+//     g_return_val_if_fail(PSY_IS_AUDIO_DEVICE(self), NULL);
+//
+//     PsyAudioDeviceClass *cls = PSY_AUDIO_DEVICE_GET_CLASS(self);
+//
+//     g_return_val_if_fail(cls->create_playback, NULL);
+//     return cls->create_playback(self);
+// }
+
+const gchar *
+psy_audio_device_get_name(PsyAudioDevice *self)
+{
+    g_return_val_if_fail(PSY_IS_AUDIO_DEVICE(self), NULL);
+    PsyAudioDevicePrivate *priv = psy_audio_device_get_instance_private(self);
+
+    return priv->name;
+}
+
+const gchar *
+psy_audio_device_get_default_name(PsyAudioDevice *self)
+{
+    g_return_val_if_fail(PSY_IS_AUDIO_DEVICE(self), NULL);
+
+    PsyAudioDeviceClass *cls = PSY_AUDIO_DEVICE_GET_CLASS(self);
+
+    g_return_val_if_fail(cls->get_default_name, NULL);
+
+    return cls->get_default_name(self);
+}
---
+++ psy/psy-audio-device.h
@@ -0,0 +1,64 @@
+#ifndef PSY_AUDIO_DEVICE_H
+#define PSY_AUDIO_DEVICE_H
+
+#include <gio/gio.h>
+
+#include <psy-matrix4.h>
+#include <psy-program.h>
+#include <psy-texture.h>
+#include <psy-vbuffer.h>
+
+G_BEGIN_DECLS
+
+#define PSY_AUDIO_DEVICE_ERROR psy_audio_device_error_quark()
+G_MODULE_EXPORT GQuark
+psy_audio_device_error_quark(void);
+
+#define PSY_TYPE_AUDIO_DEVICE psy_audio_device_get_type()
+G_DECLARE_DERIVABLE_TYPE(
+    PsyAudioDevice, psy_audio_device, PSY, AUDIO_DEVICE, GObject)
+
+typedef struct _PsyAudioDeviceClass {
+    GObjectClass parent_class;
+
+    void (*open)(PsyAudioDevice *self, GError **error);
+    void (*close)(PsyAudioDevice *self);
+    void (*set_name)(PsyAudioDevice *self, const gchar *name);
+    void (*set_sample_rate)(PsyAudioDevice *self, guint sample_rate);
+    const gchar *(*get_default_name)(PsyAudioDevice *self);
+
+    gpointer extensions[16];
+
+} PsyAudioDeviceClass;
+
+PsyAudioDevice *
+psy_audio_device_new(void);
+
+void
+psy_audio_device_open(PsyAudioDevice *self, GError **error);
+
+const gchar *
+psy_audio_device_get_name(PsyAudioDevice *self);
+
+void
+psy_audio_device_set_name(PsyAudioDevice *self, const gchar *name);
+
+guint
+psy_audio_get_sample_rate(PsyAudioDevice *self);
+
+guint
+psy_audio_set_sample_rate(PsyAudioDevice *self, guint sample_rate);
+
+gboolean
+psy_audio_device_get_is_open(PsyAudioDevice *self);
+
+const gchar *
+psy_audio_device_get_default_name(PsyAudioDevice *self);
+
+// TODO
+// PsyAudioPlayback*
+// psy_audio_device_get_playback(PsyAudioDevice* device);
+
+G_END_DECLS
+
+#endif
