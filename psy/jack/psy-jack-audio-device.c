9356a34711a8a6ace7f645f7965a65fda9df068d
Maarten Duijndam <m.j.a.duijndam@uu.nl>
2023-07-18 14:41:13 +0200

    JackAudio create and connect ports install callbacks

--- psy/jack/psy-jack-audio-device.c
+++ psy/jack/psy-jack-audio-device.c
@@ -1,6 +1,8 @@
 
 #include <jack/jack.h>
+#include <stdio.h>
 
+#include "enum-types.h"
 #include "psy-config.h"
 #include "psy-enums.h"
 #include "psy-jack-audio-device.h"
@@ -14,8 +16,9 @@
 
 typedef struct _PsyJackAudioDevice {
     PsyAudioDevice parent;
-
     jack_client_t *client;
+    GPtrArray     *capture_ports;
+    GPtrArray     *playback_ports;
 } PsyJackAudioDevice;
 
 G_DEFINE_FINAL_TYPE(PsyJackAudioDevice,
@@ -26,6 +29,183 @@ typedef enum { PROP_NULL, NUM_PROPERTIES } PsyJackAudioDeviceProperty;
 
 //
 // static GParamSpec *jack_audio_device_properties[NUM_PROPERTIES];
+
+/* *************** private methods ************** */
+
+static int
+jack_audio_device_on_process(jack_nframes_t n, void *audio_device)
+{
+    PsyAudioDevice *self = audio_device;
+    (void) n;
+
+    return 0;
+}
+
+static void
+jack_audio_device_on_shut_down(void *audio_device)
+{
+    PsyAudioDevice *self = audio_device;
+}
+
+static int
+jack_audio_device_on_sample_rate_change(jack_nframes_t n, void *audio_device)
+{
+    PsyAudioDevice *self = audio_device;
+    (void) n;
+
+    return 0;
+}
+
+static int
+jack_audio_device_on_xrun(void *audio_device)
+{
+    PsyAudioDevice *self = audio_device;
+
+    return 0;
+}
+
+static void
+jack_audio_device_on_latency(jack_latency_callback_mode_t mode,
+                             void                        *audio_device)
+{
+    PsyAudioDevice *self = audio_device;
+
+    if (mode == JackCaptureLatency) {
+        // TODO
+    }
+    else if (mode == JackPlaybackLatency) {
+        // TODO
+    }
+}
+
+static void
+jack_audio_device_on_error(const char *error)
+{
+    g_error("JackAudioDevice encountered an error: %s", error);
+}
+
+static int
+jack_audio_device_register_callbacks(PsyJackAudioDevice *self)
+{
+    jack_client_t *client = self->client; // alias
+    int            status = 0;
+    status
+        = jack_set_process_callback(client, jack_audio_device_on_process, self);
+    if (status) {
+        g_error("Unable to set process callback: %d", status);
+        return status;
+    }
+
+    jack_on_shutdown(client, jack_audio_device_on_shut_down, self);
+
+    status = jack_set_sample_rate_callback(
+        client, jack_audio_device_on_sample_rate_change, self);
+    if (status) {
+        g_error("Unable to set sample_rate callback: %d", status);
+        return status;
+    }
+
+    status = jack_set_xrun_callback(client, jack_audio_device_on_xrun, self);
+    if (status) {
+        g_error("Unable to set on_xrun callback: %d", status);
+        return status;
+    }
+
+    status
+        = jack_set_latency_callback(client, jack_audio_device_on_latency, self);
+    if (status) {
+        g_error("Unable to set on_latency callback: %d", status);
+        return status;
+    }
+
+    jack_set_error_function(jack_audio_device_on_error);
+
+    return status;
+}
+
+static int
+jack_audio_device_get_ports(PsyJackAudioDevice *self)
+{
+    jack_client_t *client = self->client; // alias
+
+    enum JackPortFlags playback_flags = JackPortIsPhysical | JackPortIsInput;
+    enum JackPortFlags capture_flags  = JackPortIsPhysical | JackPortIsOutput;
+
+    const char **capture_ports
+        = jack_get_ports(client, NULL, JACK_DEFAULT_AUDIO_TYPE, capture_flags);
+    const char **playback_ports
+        = jack_get_ports(client, NULL, JACK_DEFAULT_AUDIO_TYPE, capture_flags);
+
+    gchar port_name[1024];
+
+    for (guint i = 0;; i++) {
+        if (capture_ports[i] == NULL) {
+            break;
+        }
+        sprintf(port_name, "psy-input-%d", i);
+        jack_port_t *port = jack_port_register(
+            client, port_name, JACK_DEFAULT_AUDIO_TYPE, JackPortIsInput, 0);
+
+        g_ptr_array_add(self->capture_ports, port);
+    }
+
+    for (int i = 0;; i++) {
+        if (playback_ports[i] == NULL) {
+            break;
+        }
+        sprintf(port_name, "psy-output-%d", i);
+        jack_port_t *port = jack_port_register(
+            client, port_name, JACK_DEFAULT_AUDIO_TYPE, JackPortIsOutput, 0);
+
+        g_ptr_array_add(self->playback_ports, port);
+    }
+
+    jack_free(capture_ports);
+    jack_free(playback_ports);
+    return 0;
+}
+
+static int
+jack_audio_device_connect_ports(PsyJackAudioDevice *self)
+{
+    jack_client_t *client = self->client; // alias
+
+    enum JackPortFlags playback_flags = JackPortIsPhysical | JackPortIsInput;
+    enum JackPortFlags capture_flags  = JackPortIsPhysical | JackPortIsOutput;
+
+    const char **capture_ports
+        = jack_get_ports(client, NULL, JACK_DEFAULT_AUDIO_TYPE, capture_flags);
+    const char **playback_ports
+        = jack_get_ports(client, NULL, JACK_DEFAULT_AUDIO_TYPE, playback_flags);
+
+    int status;
+
+    for (unsigned i = 0; i < self->capture_ports->len; i++) {
+        jack_port_t *port = self->capture_ports->pdata[i];
+
+        status = jack_connect(client, capture_ports[i], jack_port_name(port));
+        if (status) {
+            g_critical("Unable to connect ports %s to %s, status is %x",
+                       capture_ports[i],
+                       jack_port_name(port),
+                       status);
+        }
+    }
+    for (unsigned i = 0; i < self->playback_ports->len; i++) {
+        jack_port_t *port = self->playback_ports->pdata[i];
+
+        status = jack_connect(client, jack_port_name(port), playback_ports[i]);
+        if (status) {
+            g_critical("Unable to connect ports %s to %s, status is %x",
+                       jack_port_name(port),
+                       playback_ports[i],
+                       status);
+        }
+    }
+    return 0;
+}
+
+/* *********** virtual methods ***************** */
 
 // static void
 // psy_jack_audio_device_set_property(GObject      *object,
@@ -65,6 +245,9 @@ static void
 psy_jack_audio_device_init(PsyJackAudioDevice *self)
 {
     self->client = NULL;
+
+    self->capture_ports  = g_ptr_array_new();
+    self->playback_ports = g_ptr_array_new();
 }
 
 static void
@@ -72,6 +255,8 @@ psy_jack_audio_device_dispose(GObject *object)
 {
     PsyJackAudioDevice *self = PSY_JACK_AUDIO_DEVICE(object);
     (void) self;
+
+    // self->jack_client is closed in psy_audio_device_close()
 
     G_OBJECT_CLASS(psy_jack_audio_device_parent_class)->dispose(object);
 }
@@ -81,9 +266,11 @@ psy_jack_audio_device_finalize(GObject *object)
 {
     PsyJackAudioDevice *self = PSY_JACK_AUDIO_DEVICE(object);
     (void) self;
-    if (psy_audio_device_get_is_open(PSY_AUDIO_DEVICE(self))) {
-        psy_audio_device_close(PSY_AUDIO_DEVICE(self));
-    }
+
+    // self->jack_client is closed in psy_audio_device_close()
+
+    g_ptr_array_free(self->capture_ports, TRUE);
+    g_ptr_array_free(self->playback_ports, TRUE);
 
     G_OBJECT_CLASS(psy_jack_audio_device_parent_class)->finalize(object);
 }
@@ -105,6 +292,33 @@ jack_audio_device_open(PsyAudioDevice *self, GError **error)
                     "Unable to connect to a jack server, has it been started?");
         return;
     }
+
+    status = jack_audio_device_register_callbacks(PSY_JACK_AUDIO_DEVICE(self));
+    if (status) {
+        g_set_error(error,
+                    psy_audio_device_error_get_type(),
+                    PSY_AUDIO_DEVICE_ERROR_FAILED,
+                    "Unable to set jack callbacks");
+        return;
+    }
+
+    // Create ports here or below register we can connect them.
+    jack_audio_device_get_ports(PSY_JACK_AUDIO_DEVICE(self));
+
+    status = jack_activate(jack_self->client);
+    if (status) {
+        g_set_error(error,
+                    psy_audio_device_error_get_type(),
+                    PSY_AUDIO_DEVICE_ERROR_FAILED,
+                    "Unable to activate client: %d",
+                    status);
+        return;
+    }
+
+    jack_audio_device_connect_ports(PSY_JACK_AUDIO_DEVICE(self));
+
+    PSY_AUDIO_DEVICE_CLASS(psy_jack_audio_device_parent_class)
+        ->open(self, error);
 }
 
 static void
@@ -112,12 +326,14 @@ jack_audio_device_close(PsyAudioDevice *self)
 {
     PsyJackAudioDevice *jack_self = PSY_JACK_AUDIO_DEVICE(self);
     jack_client_close(jack_self->client);
+    PSY_AUDIO_DEVICE_CLASS(psy_jack_audio_device_parent_class)->close(self);
 }
 
 static const gchar *
 jack_audio_device_get_default_name(PsyAudioDevice *self)
 {
     (void) self;
+    // NOTE This is only correct when using ALSA as jack backend.
     return "hw:0";
 }
 
@@ -141,7 +357,7 @@ psy_jack_audio_device_class_init(PsyJackAudioDeviceClass *klass)
     //    audio_klass->set_name        = jack_audio_device_set_name;
     //    audio_klass->set_sample_rate = jack_audio_device_set_sample_rate;
 
-    // we currently do not have properties
+    // We currently do not have properties
     //    g_object_class_install_properties(
     //        gobject_class, NUM_PROPERTIES, jack_audio_device_properties);
 }
@@ -152,8 +368,8 @@ psy_jack_audio_device_class_init(PsyJackAudioDeviceClass *klass)
  * psy_jack_audio_device_new:(constructor)
  *
  * Constructs an jack audio device.
- * This object will try to connect to or instantiate a jack server in order to
- * obtain the playback and capture devices.
+ * This object will try to connect to or instantiate a jack server in order
+ * to obtain the playback and capture devices.
  *
  * Returns: a instance of [class@PsyJackAudioDevice]
  */
