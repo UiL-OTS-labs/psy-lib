974d53de0d845434e35db906d334ee2baccd49df
Maarten Duijndam <m.j.a.duijndam@uu.nl>
2024-10-23 14:26:32 +0200

    Add utility to build and test the default builds
    The run_test.py utility is a utility that knows about 5 different
    types of builds. gcc and clang, debug and release and a build with
    -fsanitize=address,undefined enabled.
    
    This allows to run the unit-tests and examples in order to see
    if the tests and examples are happy with the current state of psylib
    
    In addition it makes the command to test everything a bit easier.
    from: meson devenv -C build meson test
    to: ./run_test.py build
    or ./run_test.py all to run all 5 tests.
    Additionally, this allows to locally setup the 5 environments relatively
    easily.

---
+++ run_test.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+
+
+import argparse as ap
+import os.path as p
+import os
+import sys
+import shutil
+import subprocess
+
+known_builds = ["debug", "release", "clang-debug", "clang-release", "sanitizers"]
+
+PATH = "PATH"
+CURRENT_PATH = os.getenv(PATH)
+
+CURRENT_ENV = os.environ.copy()
+
+BUILD_ENV = {
+    "debug": CURRENT_ENV,
+    "release": CURRENT_ENV,
+    "clang-debug": CURRENT_ENV | {"CC": "clang", "CXX": "clang++"},
+    "clang-release": CURRENT_ENV | {"CC": "clang", "CXX": "clang++"},
+    "sanitizers": CURRENT_ENV,
+}
+
+RELEASE = "--buildtype=release"
+DEBUG = "--buildtype=debug"
+MESON = "meson"
+
+BUILD_FLAGS = {
+    "debug": ["--werror", "-Ddocumentation=false"] + [DEBUG],
+    "release": ["--werror"] + [RELEASE],
+    "clang-debug": ["--werror", "-Ddocumentation=false"] + [DEBUG],
+    "clang-release": ["--werror"] + [RELEASE],
+    "sanitizers": [
+        "--werror",
+        "-Ddocumentation=false",
+        "-Db_sanitize=address,undefined",
+    ]
+    + [DEBUG],
+}
+
+
+def configure_build(folder: known_builds) -> int:
+    """configures a build folder"""
+    if folder not in known_builds:
+        raise ValueError(f"Oops: {folder} is not a know build")
+    return subprocess.run(
+        [MESON, "setup", folder] + BUILD_FLAGS[folder], env=BUILD_ENV[folder]
+    ).returncode
+
+
+def build_folder(folder) -> int:
+    """build the current build in the folder"""
+    run_result = subprocess.run(
+        [MESON, "compile", "-C", folder],
+        env=BUILD_ENV[folder],
+    )
+    return run_result.returncode
+
+
+def test_folder(folder) -> int:
+    """Runs the unit tests"""
+    run_result = subprocess.run(
+        [MESON, "devenv", "-C", folder, MESON, "test"],
+        env=BUILD_ENV[folder],
+    )
+    return run_result.returncode
+
+
+def configure_build_and_test(folder: str) -> int:
+    if not p.isdir(folder):
+        ret = configure_build(folder)
+        if ret != 0:
+            print(f"Oops unable to configure: {folder}", file=sys.stderr)
+            exit(ret)
+        ret = build_folder(folder)
+        if ret != 0:
+            print(f"Oops unable to build: {folder}", file=sys.stderr)
+            exit(ret)
+
+    return test_folder(folder)
+
+
+if __name__ == "__main__":
+    parser = ap.ArgumentParser("test.py", description="run one or multiple tests")
+    parser.add_argument(
+        "builds", nargs="*", default=["debug"], help="specify the folder of the build"
+    )
+
+    np = parser.parse_args()
+
+    builds = np.builds
+    if builds == ["all"]:
+        builds = known_builds
+
+    for build in builds:
+        ret = 0
+        if build in known_builds:
+            ret = configure_build_and_test(build)
+            if ret != 0:
+                exit(ret)
+
+        if p.isdir(build) and p.isfile(p.join(build, "ninja.build")):
+            test_folder(build)
+
+    print(f"Builds: {builds} completed")
