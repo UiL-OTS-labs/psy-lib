3f9fe9994e7c84db786e77a1cc492bba586ad9b8
Maarten Duijndam <m.j.a.duijndam@uu.nl>
2024-10-23 19:42:27 +0200

    Add test PsyTimer accuracy

--- tests/test-timer.c
+++ tests/test-timer.c
@@ -94,14 +94,14 @@ typedef struct {
 static gboolean
 on_timer_fire1(PsyTimer *t, PsyTimePoint *tp, gpointer data)
 {
-    TimerFireTest *fire_test = data;
+    TimerFireTest *fire_data = data;
 
-    fire_test->fired = TRUE;
-    fire_test->time_is_equal_to_set
-        = psy_time_point_equal(fire_test->time_in, tp);
-    fire_test->timer_is_the_same = fire_test->timer_in == t;
+    fire_data->fired = TRUE;
+    fire_data->time_is_equal_to_set
+        = psy_time_point_equal(fire_data->time_in, tp);
+    fire_data->timer_is_the_same = fire_data->timer_in == t;
 
-    g_main_loop_quit(fire_test->utils->loop);
+    g_main_loop_quit(fire_data->utils->loop);
 
     return G_SOURCE_REMOVE;
 }
@@ -122,7 +122,8 @@ test_timer_fire(void)
     g_object_set(t1, "fire-time", now, NULL);
     g_signal_connect(t1, "fired", G_CALLBACK(on_timer_fire1), &test_data);
 
-    g_timeout_add(10, G_SOURCE_FUNC(g_main_loop_quit), utils->loop);
+    guint source_id
+        = g_timeout_add(10, G_SOURCE_FUNC(g_main_loop_quit), utils->loop);
 
     CU_ASSERT_PTR_NOT_NULL(psy_timer_get_fire_time(t1));
 
@@ -132,7 +133,76 @@ test_timer_fire(void)
     CU_ASSERT_TRUE(test_data.time_is_equal_to_set);
     CU_ASSERT_TRUE(test_data.timer_is_the_same);
 
+    // otherwise a new context might cancel this mainloop
+    gboolean removed = g_source_remove(source_id);
+    if (!removed)
+        g_critical("unable to remove timeout source");
+    g_assert(removed);
+
     psy_time_point_free(now);
+    psy_timer_free(t1);
+    timer_test_utilities_free(utils);
+}
+
+typedef struct {
+    TimerTestUtilities *utils;
+    PsyTimePoint       *fire_time;
+} TimerFireAccuratelyTest;
+
+static gboolean
+on_timer_fire_accurately(PsyTimer *t, PsyTimePoint *tp, gpointer data)
+{
+    (void) t;
+    (void) tp;
+    TimerFireAccuratelyTest *fire_data = data;
+
+    // Must be freed
+    fire_data->fire_time = psy_clock_now(fire_data->utils->clk);
+
+    g_main_loop_quit(fire_data->utils->loop);
+
+    return G_SOURCE_REMOVE;
+}
+
+static void
+test_timer_fire_accurately(void)
+{
+    TimerTestUtilities *utils       = timer_test_utilities_new();
+    PsyTimer           *t1          = NULL;
+    PsyTimePoint       *now         = psy_clock_now(utils->clk);
+    PsyDuration        *dur         = psy_duration_new_ms(10);
+    PsyDuration        *time_diff   = NULL;
+    PsyTimePoint       *time_future = psy_time_point_add(now, dur);
+
+    t1 = psy_timer_new();
+
+    TimerFireAccuratelyTest test_data = {utils, NULL};
+
+    CU_ASSERT_PTR_NOT_NULL_FATAL(t1);
+
+    g_object_set(t1, "fire-time", time_future, NULL);
+    g_signal_connect(
+        t1, "fired", G_CALLBACK(on_timer_fire_accurately), &test_data);
+
+    g_main_loop_run(utils->loop);
+
+    // At this point time_future has ellapsed to the past...
+    time_diff = psy_time_point_subtract(test_data.fire_time, time_future);
+    psy_time_point_free(test_data.fire_time);
+
+    // We expect that a timer is not fired ahead of time.
+    CU_ASSERT_TRUE(psy_duration_get_us(time_diff) >= 0);
+    // We expect that a timer is not fired too late e.g. more than one ms
+    CU_ASSERT_TRUE(psy_duration_get_us(time_diff) < 1000);
+    g_info("The timer was fired at %" PRId64 " us",
+           psy_duration_get_us(time_diff));
+
+    psy_duration_free(time_diff);
+    psy_time_point_free(time_future);
+    psy_duration_free(dur);
+    psy_time_point_free(now);
+    psy_timer_free(t1);
+
     timer_test_utilities_free(utils);
 }
 
@@ -154,6 +224,10 @@ add_timer_suite(void)
         return 1;
 
     test = CU_ADD_TEST(suite, test_timer_fire);
+    if (!test)
+        return 1;
+
+    test = CU_ADD_TEST(suite, test_timer_fire_accurately);
     if (!test)
         return 1;
 
