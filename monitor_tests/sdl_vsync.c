e71cfa71ff5acef0e2027316f10d6f0c7e2f5fee
Maarten Duijndam <m.j.a.duijndam@uu.nl>
2025-01-20 10:08:18 +0100

    SDL vsync test: Draw a rect with raw opengl

--- monitor_tests/sdl_vsync.c
+++ monitor_tests/sdl_vsync.c
@@ -1,12 +1,16 @@
 
+#define G_LOG_DOMAIN "SdlVSync"
+
 #include <psylib.h>
 
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_error.h>
 #include <SDL3/SDL_events.h>
 #include <SDL3/SDL_opengl.h>
+#include <assert.h>
 
 #include "cmd_vsync_opts.h"
+#include "monitor_shader_paths.h"
 
 const SDL_DisplayMode *
 get_display_mode_for_monitor(int nth_display, SDL_Rect *out)
@@ -35,15 +39,140 @@ cleanup:
     return ret;
 }
 
+PsyShaderProgram *
+init_shaders(void)
+{
+    PsyShaderProgram *prog  = PSY_SHADER_PROGRAM(psy_gl_program_new());
+    GError           *error = NULL;
+
+    char vert_shader[1024];
+    char frag_shader[1024];
+
+    g_snprintf(
+        vert_shader, 1024, "%s/%s", PSY_BUILD_PSY_DIR, "uniform-color.vert");
+    g_snprintf(
+        frag_shader, 1024, "%s/%s", PSY_BUILD_PSY_DIR, "uniform-color.frag");
+
+    g_info("Vert shader path = %s", vert_shader);
+    g_info("Frag shader path = %s", frag_shader);
+
+    if (!prog) {
+        g_critical("Unable to create shader program");
+        return NULL;
+    }
+
+    psy_shader_program_set_vertex_shader_from_path(prog, vert_shader, &error);
+    if (error) {
+        g_critical("Unable to set shader path: %s", error->message);
+        g_clear_error(&error);
+        goto error;
+    }
+    psy_shader_program_set_fragment_shader_from_path(prog, frag_shader, &error);
+    if (error) {
+        g_critical("Unable to set shader path: %s", error->message);
+        g_clear_error(&error);
+        goto error;
+    }
+
+    psy_shader_program_link(prog, &error);
+    if (error) {
+        g_critical("Unable to link program: %s", error->message);
+        g_clear_error(&error);
+        goto error;
+    }
+
+    return prog;
+
+error:
+    g_clear_object(&prog);
+    return prog;
+}
+
 void
 render_loop(SDL_Window *win)
 {
-    int running = 1;
+    int     running  = 1;
+    GError *error    = NULL;
+    float   white[4] = {1.0f, 1.0f, 1.0f, 1.0f};
+    float   sw       = 100; // square_width
 
     SDL_GLContext context = SDL_GL_CreateContext(win);
     SDL_GL_MakeCurrent(win, context);
+    int w, h;
+    SDL_GetWindowSizeInPixels(win, &w, &h);
 
-    PsyClock     *clk  = psy_clock_new();
+    glViewport(0, 0, w, h);
+
+    PsyMatrix4 *projection = psy_matrix4_new_ortographic(
+        -w / 2.0, w / 2.0f, -h / 2.0f, h / 2.0f, 0.0f, 100.0f);
+    PsyMatrix4 *model = psy_matrix4_new_identity();
+
+    PsyShaderProgram *program = init_shaders();
+    if (!program) {
+        g_critical("Unable to create shader program");
+        goto error;
+    }
+    psy_shader_program_use(program, &error);
+    if (error) {
+        g_critical("Unable to use program: %s\n", error->message);
+        goto error;
+    }
+    psy_shader_program_set_uniform_4f(program, "ourColor", white, &error);
+    if (error) {
+        g_critical("Unable to set color: %s", error->message);
+        goto error;
+    }
+    psy_shader_program_set_uniform_matrix4(
+        program, "projection", projection, &error);
+    if (error) {
+        g_critical("Unable to set projection matrix: %s", error->message);
+        goto error;
+    }
+    psy_shader_program_set_uniform_matrix4(program, "model", model, &error);
+    if (error) {
+        g_critical("Unable to set view matrix: %s", error->message);
+        goto error;
+    }
+    psy_matrix4_free(projection);
+    psy_matrix4_free(model);
+
+    PsyGlVBuffer *vbuffer = psy_gl_vbuffer_new();
+
+    // clang-format off
+
+    PsyVertex verts[] = {
+        {
+            .pos = {sw / 2.0f, sw / 2.0f, 0.0f},
+        },
+        {
+            .pos = {-sw / 2.0f, sw/2.0f, 0.0f},
+        },
+        {
+            .pos = {-sw / 2, -sw/2, 0},
+        },
+        {
+            .pos = {sw / 2.0f, -sw / 2.0f, 0},
+        },
+    };
+
+    // clang-format on
+    psy_vbuffer_set_from_data(
+        PSY_VBUFFER(vbuffer), verts, sizeof(verts) / sizeof(verts[0]));
+
+    PsyClock *clk = psy_clock_new();
+
+    FILE *outfile = fopen("sdl_vsync.txt", "wb");
+
+    uint64_t start = SDL_GetTicks();
+
+    while (true) { // wait a half of a second for the vsync to stabilize.
+        glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        SDL_GL_SwapWindow(win);
+        if ((SDL_GetTicks() - start) > 500)
+            break;
+    }
+
     PsyTimePoint *last = psy_clock_now(clk);
 
     while (running) {
@@ -52,21 +181,54 @@ render_loop(SDL_Window *win)
         while (SDL_PollEvent(&e) == true) {
             if (e.type == SDL_EVENT_QUIT) {
                 running = false;
+                g_debug("Quit event encountered");
+            }
+            if (e.type == SDL_EVENT_WINDOW_RESIZED) {
+                gint width  = e.display.data1;
+                gint height = e.display.data2;
+                g_debug("Setting viewport to 0 0 %d %d", width, height);
+
+                glViewport(0, 0, width, height);
+                PsyMatrix4 *p = psy_matrix4_new_ortographic(-width / 2.0f,
+                                                            width / 2.0f,
+                                                            -height / 2.0f,
+                                                            height / 2.0f,
+                                                            0.f,
+                                                            100.f);
+                psy_shader_program_set_uniform_matrix4(
+                    program, "projection", p, &error);
+                if (error) {
+                    g_critical("Unable to set projection matrix: %s",
+                               error->message);
+                }
+                psy_matrix4_free(p);
+
+                // error handing on glViewPort
+                assert(glGetError() == GL_NO_ERROR);
             }
         }
 
         glClearColor(0.0, 0.0, 0.0, 1.0);
         glClear(GL_COLOR_BUFFER_BIT);
 
+        psy_vbuffer_draw_triangle_fan(PSY_VBUFFER(vbuffer), &error);
+        if (error) {
+            g_critical("Unable to draw triangle string: %s", error->message);
+            goto error;
+        }
+
         SDL_GL_SwapWindow(win);
         PsyTimePoint *temp = psy_clock_now(clk);
         PsyDuration  *dur  = psy_time_point_subtract(temp, last);
-        // g_print("Time since last swap = %lfs\n",
-        // psy_duration_get_seconds(dur));
+        fprintf(outfile, "%lf\n", psy_duration_get_seconds(dur));
         psy_duration_free(dur);
         psy_time_point_free(last);
         last = temp;
     }
+
+error:
+
+    fclose(outfile);
 
     SDL_GL_DestroyContext(context);
 
@@ -81,6 +243,10 @@ main(int argc, char **argv)
 
     cmd_parse(&argc, &argv);
     const CmdOptions *opts = cmd_get_options();
+    if (!opts) {
+        ret = EXIT_FAILURE;
+        goto opt_error;
+    }
 
     SDL_Init(SDL_INIT_VIDEO);
     PsyInitializer *psy_init = g_object_new(
@@ -88,8 +254,17 @@ main(int argc, char **argv)
 
     SDL_Window *win = NULL;
 
-    win = SDL_CreateWindow(
-        "Psy test SDL_Window timing", 640, 480, SDL_WINDOW_OPENGL);
+    SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 16);
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_PROFILE_CORE);
+
+    win = SDL_CreateWindow("Psy test SDL_Window timing",
+                           640,
+                           480,
+                           SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
     if (!win) {
         g_critical("Oops unable to create window: %s", SDL_GetError());
         ret = EXIT_FAILURE;
@@ -105,15 +280,10 @@ main(int argc, char **argv)
         g_critical("Unable to set window position: %s", SDL_GetError());
     }
 
-    if (!SDL_SetWindowFullscreen(win, true)) {
-        g_critical("Unable to set window fullscreen");
-    }
-
-    //    if (!SDL_SetWindowFullscreenMode(win, mode)) {
-    //        g_critical("Unable to set window fullscreen: %s", SDL_GetError());
-    //    }
-    if (!SDL_ShowWindow(win)) {
-        g_critical("Unable to show window: %s", SDL_GetError());
+    if (opts->fullscreen) {
+        if (!SDL_SetWindowFullscreen(win, true)) {
+            g_critical("Unable to set window fullscreen");
+        }
     }
 
     render_loop(win);
@@ -121,5 +291,6 @@ main(int argc, char **argv)
 error:
     psy_initializer_free(psy_init);
     SDL_Quit();
+opt_error:
     return ret;
 }
