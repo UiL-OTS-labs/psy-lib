b8e8010ee2c78a8b6c29a6716c0ed54bf8578614
Maarten Duijndam <m.j.a.duijndam@uu.nl>
2026-05-18 20:01:47 +0200

    Add test init and image

--- tests/test-image.c
+++ tests/test-image.c
@@ -1,30 +1,11 @@
 
 #include <math.h>
-#include <string.h>
-
-#include "unit-test-utilities.h"
-#include <criterion/criterion.h>
-#include <criterion/new/assert.h>
-
 #include <psylib.h>
 
-static void
-image_setup(void)
-{
-    set_log_handler_file("test-image.txt");
-    init_random();
-}
+#include "unit-test-utilities.h"
 
 static void
-image_teardown(void)
-{
-    deinitialize_random();
-    set_log_handler_file(NULL);
-}
-
-TestSuite(image, .init = image_setup, .fini = image_teardown);
-
-Test(image, create1)
+test_image_create1(void)
 {
     const guint          WIDTH = 1920, HEIGHT = 1080, NCHANNELS = 4;
     const PsyImageFormat format = PSY_IMAGE_FORMAT_RGBA;
@@ -36,7 +17,7 @@ Test(image, create1)
 
     PsyImage *img = psy_image_new(WIDTH, HEIGHT, format);
 
-    cr_assert(ne(img, NULL));
+    g_assert_nonnull(img);
 
     // clang-format off
     g_object_get(img,
@@ -50,16 +31,17 @@ Test(image, create1)
             );
     // clang-format on
 
-    cr_expect(eq(width, WIDTH));
-    cr_expect(eq(height, HEIGHT));
-    cr_expect(eq(nchannels, NCHANNELS));
-    cr_expect(eq(nbytes, WIDTH * HEIGHT * NCHANNELS));
-    cr_expect(eq(stride, WIDTH * NCHANNELS));
+    g_assert_cmpuint(width, ==, WIDTH);
+    g_assert_cmpuint(height, ==, HEIGHT);
+    g_assert_cmpuint(nchannels, ==, NCHANNELS);
+    g_assert_cmpuint(nbytes, ==, WIDTH * HEIGHT * NCHANNELS);
+    g_assert_cmpuint(stride, ==, WIDTH * NCHANNELS);
 
     g_object_unref(img);
 }
 
-Test(image, create2)
+static void
+test_image_create2(void)
 {
     const guint          WIDTH = 1280, HEIGHT = 640, NCHANNELS = 3;
     const PsyImageFormat format = PSY_IMAGE_FORMAT_RGB;
@@ -71,7 +53,7 @@ Test(image, create2)
 
     PsyImage *img = psy_image_new(WIDTH, HEIGHT, format);
 
-    cr_assert(ne(img, NULL));
+    g_assert_nonnull(img);
 
     // clang-format off
     g_object_get(img,
@@ -85,106 +67,116 @@ Test(image, create2)
             );
     // clang-format on
 
-    cr_expect(eq(width, WIDTH));
-    cr_expect(eq(height, HEIGHT));
-    cr_expect(eq(nchannels, NCHANNELS));
-    cr_expect(eq(nbytes, WIDTH * HEIGHT * NCHANNELS));
-    cr_expect(eq(stride, WIDTH * NCHANNELS));
-    cr_expect(eq(format_out, format));
+    g_assert_cmpuint(width, ==, WIDTH);
+    g_assert_cmpuint(height, ==, HEIGHT);
+    g_assert_cmpuint(nchannels, ==, NCHANNELS);
+    g_assert_cmpuint(nbytes, ==, (guint64) WIDTH * HEIGHT * NCHANNELS);
+    g_assert_cmpuint(stride, ==, WIDTH * NCHANNELS);
+    g_assert_cmpuint(format_out, ==, format);
 
     g_object_unref(img);
 }
 
-Test(image, change_format)
+static void
+test_image_change_format(void)
 {
     const guint WIDTH = 1280, HEIGHT = 640;
 
     PsyImage *img = psy_image_new(WIDTH, HEIGHT, PSY_IMAGE_FORMAT_RGB);
 
-    cr_assert(ne(img, NULL));
+    g_assert_nonnull(img);
 
-    cr_expect(eq(psy_image_pixel_num_bytes(img), 3u));
-    cr_expect(eq(psy_image_get_num_bytes(img),
-                 WIDTH * HEIGHT * psy_image_pixel_num_bytes(img)));
+    g_assert_cmpuint(psy_image_pixel_num_bytes(img), ==, 3u);
+    g_assert_cmpuint(psy_image_get_num_bytes(img),
+                     ==,
+                     WIDTH * HEIGHT * psy_image_pixel_num_bytes(img));
 
     psy_image_set_format(img, PSY_IMAGE_FORMAT_RGBA);
 
-    cr_expect(eq(psy_image_pixel_num_bytes(img), 4u));
-    cr_expect(eq(psy_image_get_num_bytes(img),
-                 WIDTH * HEIGHT * psy_image_pixel_num_bytes(img)));
+    g_assert_cmpuint(psy_image_pixel_num_bytes(img), ==, 4u);
+    g_assert_cmpuint(psy_image_get_num_bytes(img),
+                     ==,
+                     WIDTH * HEIGHT * psy_image_pixel_num_bytes(img));
 
     g_object_unref(img);
 }
 
-Test(image, clear)
+static void
+test_image_clear(void)
 {
     const guint WIDTH = 100, HEIGHT = 100;
 
     PsyImage *img = psy_image_new(WIDTH, HEIGHT, PSY_IMAGE_FORMAT_RGB);
-    cr_assert(ne(img, NULL));
+    g_assert_nonnull(img);
 
-    PsyColor *bg = psy_color_new_rgb(random_double_range(0, 1),
-                                     random_double_range(0, 1),
-                                     random_double_range(0, 1));
-    cr_assert(ne(bg, NULL));
+    PsyColor *bg = psy_color_new_rgb((float) g_test_rand_double_range(0, 1),
+                                     (float) g_test_rand_double_range(0, 1),
+                                     (float) g_test_rand_double_range(0, 1));
+    g_assert_nonnull(bg);
 
     psy_image_clear(img, bg);
 
-    PsyColor *probe = psy_image_get_pixel(
-        img, random_int_range(0, WIDTH), random_int_range(0, HEIGHT));
+    PsyColor *probe
+        = psy_image_get_pixel(img,
+                              g_test_rand_int_range(0, (gint) WIDTH),
+                              g_test_rand_int_range(0, (gint) HEIGHT));
 
-    cr_assert(psy_color_equal_eps(probe, bg, 1 / 255.0));
+    g_assert_true(psy_color_equal_eps(probe, bg, 1 / 255.0));
     psy_color_free(probe);
 
     // clang-format off
     g_object_set(bg,
-            "r", random_double_range(0, 1),
-            "b", random_double_range(0, 1),
-            "g", random_double_range(0, 1),
-            "a", random_double_range(0, 1),
+            "r", (float)g_test_rand_double_range(0, 1),
+            "b", (float)g_test_rand_double_range(0, 1),
+            "g", (float)g_test_rand_double_range(0, 1),
+            "a", (float)g_test_rand_double_range(0, 1),
             NULL);
     // clang-format on
     g_object_set(img, "format", PSY_IMAGE_FORMAT_RGBA, NULL);
     psy_image_clear(img, bg);
 
-    probe = psy_image_get_pixel(
-        img, random_int_range(0, WIDTH), random_int_range(0, HEIGHT));
-    cr_assert(psy_color_equal_eps(probe, bg, 1 / 255.0));
+    probe
+        = psy_image_get_pixel(img,
+                              (guint) g_test_rand_int_range(0, (gint) WIDTH),
+                              (guint) g_test_rand_int_range(0, (gint) HEIGHT));
+    g_assert_true(psy_color_equal_eps(probe, bg, 1 / 255.0));
 
     psy_color_free(bg);
     psy_color_free(probe);
     psy_image_free(img);
 }
 
-Test(image, set_pixel)
+static void
+test_image_set_pixel(void)
 {
     const gint WIDTH = 100, HEIGHT = 100;
 
     PsyImage *img   = psy_image_new(WIDTH, HEIGHT, PSY_IMAGE_FORMAT_RGB);
-    PsyColor *color = psy_color_new_rgb((float) random_double_range(0, 1),
-                                        (float) random_double_range(0, 1),
-                                        (float) random_double_range(0, 1));
-    gint      row   = random_int_range(0, HEIGHT);
-    gint      col   = random_int_range(0, WIDTH);
+    PsyColor *color = psy_color_new_rgb((float) g_test_rand_double_range(0, 1),
+                                        (float) g_test_rand_double_range(0, 1),
+                                        (float) g_test_rand_double_range(0, 1));
+    gint      row   = g_test_rand_int_range(0, (gint) HEIGHT);
+    gint      col   = g_test_rand_int_range(0, (gint) WIDTH);
     psy_image_set_pixel(img, row, col, color);
 
     PsyColor *probe = psy_image_get_pixel(img, row, col);
 
-    cr_assert(psy_color_equal_eps(color, probe, 1.0 / 255));
+    g_assert_true(psy_color_equal_eps(color, probe, 1.0 / 255));
 
     psy_color_free(probe);
     psy_color_free(color);
     psy_image_free(img);
 }
 
-Test(image, get_bytes)
+static void
+test_image_get_bytes(void)
 {
     const gint WIDTH = 100, HEIGHT = 100;
 
     PsyImage *img   = psy_image_new(WIDTH, HEIGHT, PSY_IMAGE_FORMAT_RGB);
-    PsyColor *color = psy_color_new_rgb((float) random_double_range(0, 1),
-                                        (float) random_double_range(0, 1),
-                                        (float) random_double_range(0, 1));
+    PsyColor *color = psy_color_new_rgb((float) g_test_rand_double_range(0, 1),
+                                        (float) g_test_rand_double_range(0, 1),
+                                        (float) g_test_rand_double_range(0, 1));
     psy_image_clear(img, color);
 
     gsize bytes_size;
@@ -194,13 +186,34 @@ Test(image, get_bytes)
     const guint8 *bytes_ptr = g_bytes_get_data(bytes, &bytes_size);
     const guint8 *img_ptr   = psy_image_get_ptr(img);
 
-    cr_assert(eq(img_size, bytes_size));
+    g_assert_cmpuint(img_size, ==, bytes_size);
     // memcmp should return 0 when memory is equal... like strcmp.
-    cr_assert(eq(memcmp(img_ptr, bytes_ptr, bytes_size), 0));
+    g_assert_cmpmem(img_ptr, bytes_size, bytes_ptr, bytes_size);
     // A deep copy should be made
-    cr_assert(ne(img_ptr, bytes_ptr));
+    g_assert_true(img_ptr != bytes_ptr);
 
     psy_color_free(color);
     psy_image_free(img);
     g_bytes_unref(bytes);
+}
+
+int
+main(int argc, char **argv)
+{
+    set_log_handler_file("test-image.txt");
+
+    g_test_init(&argc, &argv, NULL);
+
+    g_test_add_func("/image/create1", test_image_create1);
+    g_test_add_func("/image/create2", test_image_create2);
+    g_test_add_func("/image/change_format", test_image_change_format);
+    g_test_add_func("/image/clear", test_image_clear);
+    g_test_add_func("/image/set_pixel", test_image_set_pixel);
+    g_test_add_func("/image/get_bytes", test_image_get_bytes);
+
+    int save = g_test_run();
+
+    set_log_handler_file(NULL);
+
+    return save;
 }
--- tests/test-init.c
+++ tests/test-init.c
@@ -1,84 +1,28 @@
 
 #include "unit-test-utilities.h"
 
-#include <criterion/criterion.h>
-#include <criterion/new/assert.h>
-#include <criterion/parameterized.h>
 #include <psylib.h>
 
 static void
-initializer_setup(void)
-{
-    install_log_handler();
-    set_log_handler_level(G_LOG_LEVEL_DEBUG);
-    set_log_handler_file("test-init.txt");
-}
-
-static void
-initializer_teardown(void)
-{
-    set_log_handler_file(NULL);
-    remove_log_handler();
-}
-
-Test(init,
-     init_default,
-     .init = initializer_setup,
-     .fini = initializer_teardown)
+test_init(void)
 {
     gboolean        gst, portaudio;
     PsyInitializer *initialzer = g_object_new(PSY_TYPE_INITIALIZER, NULL);
 
     g_object_get(initialzer, "gstreamer", &gst, "portaudio", &portaudio, NULL);
 
-    cr_assert(eq(int, gst, TRUE), "gstreamer should be initialized by default");
-    cr_assert(eq(int, portaudio, TRUE),
-              "portaudio should be initialized by default");
+    g_assert_true(gst);
+    g_assert_true(portaudio);
 
     g_object_unref(initialzer);
 }
 
 static void
-cr_free_strings(struct criterion_test_params *crp)
-{
-    char **strings = crp->params;
-    for (size_t i = 0; i < crp->length; i++) {
-        cr_free(strings[i]);
-    }
-    cr_free(strings);
-}
-
-static char *
-cr_strdup(const char *string)
-{
-    char *ret = cr_calloc(strlen(string) + 1, sizeof(string[0]));
-    ret       = strcpy(ret, string);
-    return ret;
-}
-
-ParameterizedTestParameters(init, init_specific)
-{
-    const char  *params[] = {"portaudio", "gstreamer"};
-    const size_t n_params = sizeof(params) / sizeof(params[0]);
-    char       **strings  = cr_malloc(n_params * sizeof(char *));
-
-    for (size_t i = 0; i < n_params; i++) {
-        strings[i] = cr_strdup(params[i]);
-    }
-
-    return cr_make_param_array(
-        char *, strings, sizeof(params) / sizeof(params[0]), cr_free_strings);
-}
-
-ParameterizedTest(const char **param,
-                  init,
-                  init_specific,
-                  .init = initializer_setup,
-                  .fini = initializer_teardown)
+test_init_without_backend(const void *param)
 {
     gboolean gst, portaudio;
 
-    const char *backend = *param;
+    const char *backend = param;
 
     // No need to say we do "need" gstreamer. Initialization is everything
     // unless explictly turned off as portaudio here below.
@@ -88,13 +32,27 @@ ParameterizedTest(const char **param,
     g_object_get(initialzer, "gstreamer", &gst, "portaudio", &portaudio, NULL);
 
     if (strcmp(backend, "gstreamer") == 0) {
-        cr_expect(eq(int, portaudio, TRUE), "Port audio should be on");
-        cr_assert(eq(int, gst, FALSE), "Gstreamer should be off");
+        g_assert_cmpint(portaudio, ==, TRUE);
+        g_assert_cmpint(gst, ==, FALSE);
     }
     else if (strcmp(backend, "portaudio") == 0) {
-        cr_expect(eq(int, portaudio, FALSE), "Port audio should be off");
-        cr_assert(eq(int, gst, TRUE), "Gstreamer should be on");
+        g_assert_cmpint(portaudio, ==, FALSE);
+        g_assert_cmpint(gst, ==, TRUE);
     }
 
     g_object_unref(initialzer);
+}
+
+int
+main(int argc, char **argv)
+{
+    g_test_init(&argc, &argv, NULL);
+
+    g_test_add_func("/init/default", test_init);
+    g_test_add_data_func(
+        "/init/without-pa", "portaudio", test_init_without_backend);
+    g_test_add_data_func(
+        "/init/without-gst", "gstreamer", test_init_without_backend);
+
+    return g_test_run();
 }
